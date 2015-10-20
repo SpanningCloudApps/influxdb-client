cdaf0727d56ecfbd08304b638fd0ae0181302cd8
Brandon Worcester <brandon.worcester@spanning.com>
2015-10-20 18:43:04 -0500

    Add integration test for InfluxDbHttpClient

---
+++ src/test/java/com/spanning/influxdb/test/IntegrationTest.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+package com.spanning.influxdb.test;
+
+/**
+ * Marker interface used with the {@link org.junit.experimental.categories.Category} annotation to mark a JUnit test
+ * as an integration test.
+ */
+public interface IntegrationTest {}
